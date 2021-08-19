c441674ebbd2586799702c3fb05a8a08dd81b2de
sdelon <delon.steph@gmail.com>
2021-08-19 14:26:05 +0200

    wip: a11ty

--- src/lib/Header/Index.svelte
+++ src/lib/Header/Index.svelte
@@ -5,7 +5,7 @@
 
 <header id="header" class="bg-rose-light md:pb-12 relative">
     <div class="flex flex-col justify-center items-center max-w-screen-lg xl:max-w-screen-xl mx-auto md:px-6 md:py-3 relative z-20">
-        <div class="flex items-center mb-4" aria-label="Christelle Peuziat">
+        <div class="flex items-center mb-4">
             <Logo color="#171717"/>
         </div>
         <div class="hidden md:block w-full">
